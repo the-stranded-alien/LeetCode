d803fa123fc9630a84b2d6cf08838a0301ba430b
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2022-01-11 20:20:04 +0530

    LeetCode Practice

---
+++ 0001_0100/075_Sort_Colors.cpp
@@ -0,0 +1,117 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// Given an array nums with n objects colored red, white, or blue, sort them in-place
+// so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+// We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+// You must solve this problem without using the library's sort function.
+
+// Dutch National Flag Algorithm
+// 0 - str : 0
+// str - mid : 1
+// mid - end : numbers
+// end - (n - 1) : 2
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+    	int str = 0;
+    	int mid = 0;
+    	int itr = 0;
+    	int end = nums.size() - 1;
+    	while(mid <= end) {
+    		if(nums[mid] == 0) swap(nums[mid++], nums[str++]);
+    		else if(nums[mid] == 1) mid++;
+    		else swap(nums[mid], nums[end--]);
+    	}
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	int n;
+	cin >> n;
+	vi nums;
+	nums.reserve(300);
+	int temp;
+	while(n--) {
+		cin >> temp;
+		nums.pb(temp);
+	}	
+	Solution *soln = new Solution();
+	soln->sortColors(nums);
+	for(auto num : nums) cout << num << " ";
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
