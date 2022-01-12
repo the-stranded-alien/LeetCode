78aeff09836b03206cbcb2f257a16d77a72d068c
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2022-01-12 22:55:33 +0530

    LeetCode Practice

---
+++ 0001_0100/074_Search_a_2D_Matrix.cpp
@@ -0,0 +1,121 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// Write an efficient algorithm that searches for a value in an m x n matrix.
+// This matrix has the following properties:
+// 1. Integers in each row are sorted from left to right.
+// 2. The first integer of each row is greater than the last integer of the previous row.
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int i = 0;
+        while((i >= 0) && (i < m)) {
+        	if(matrix[i][n - 1] >= target) break;
+        	else {
+        		if((i + 1) < m) i++;
+        		else break;
+        	}
+        }
+        for(int j = 0; j < n; j++) {
+        	if(matrix[i][j] == target) return true;
+        }
+        return false;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	int m;
+	cin >> m;
+	int n;
+	cin >> n;
+	vvi matrix(m, vector<int>(n, 0));
+	for(int i = 0; i < m; i++) {
+		for(int j = 0; j < n; j++) {
+			cin >> matrix[i][j];
+		}
+	}
+	int target;
+	cin >> target;
+	Solution *soln = new Solution();
+	bool isPresent = soln->searchMatrix(matrix, target);	
+	if(isPresent) cout << "true" << nl;
+	else cout << "false" << nl;
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
