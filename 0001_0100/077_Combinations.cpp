485b4e355a3d18a22ad99252f6c0e56b75a7538a
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2022-01-12 20:20:28 +0530

    LeetCode Practice

---
+++ 0001_0100/077_Combinations.cpp
@@ -0,0 +1,120 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// Given two integers n and k, return all possible
+// combinations of k numbers out of the range [1, n].
+// You may return the answer in any order.
+
+class Solution {
+public:
+	void combinationHelper(int n, int k, int idx, vector<int> &cur, vector<vector<int>> &combinations, int itr, int last) {
+		if(idx == k) {
+			combinations.push_back(cur);
+			return;
+		}
+		for(int i = itr; i <= n; i++) {
+			if(i > last) {
+				cur.push_back(i);
+				combinationHelper(n, k, idx + 1, cur, combinations, itr + 1, i);
+				cur.pop_back();	
+			} 
+			else continue;
+		}
+		return;
+	}
+
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> combinations;
+        vector<int> cur;
+        combinationHelper(n, k, 0, cur, combinations, 1, 0);
+        return combinations;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	int n; cin >> n;
+	int k; cin >> k;
+	Solution *soln = new Solution();
+	vvi combinations = soln->combine(n, k);	
+	for(auto combination : combinations) {
+		for(auto element : combination) {
+			cout << element << " ";
+		}
+		cout << nl;
+	}
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
