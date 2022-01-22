c79b7b42ffbb200efc9bf65a1d4feb8722386fe8
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2022-01-22 20:14:16 +0530

    LeetCode Practice

---
+++ 0201_0300/268_Missing_Number.cpp
@@ -0,0 +1,101 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// Given an array nums containing n distinct numbers in the range [0, n],
+// return the only number in the range that is missing from the array.
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+		int n = nums.size();
+		int sum = 0;
+		for(int i = 0; i < n; i++) sum += nums[i];
+		int targetSum = (n * (n + 1)) / 2;
+		if(targetSum - sum == 0) return 0;
+		else return(targetSum - sum);        
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	int n; cin >> n;
+	vi nums(n, 0);
+	for(int i = 0; i < n; i++) cin >> nums[i];
+	Solution *soln = new Solution();
+	int missing = soln->missingNumber(nums);
+	cout << missing << nl;
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
