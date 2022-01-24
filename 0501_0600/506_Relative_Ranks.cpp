03e278ba32d16e9e1d3183c1a8fdcc36b79a0d2a
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2022-01-24 17:53:26 +0530

    LeetCode Practice

---
+++ 0501_0600/506_Relative_Ranks.cpp
@@ -0,0 +1,117 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// You are given an integer array score of size n, where score[i] is the score of the ith athlete in a competition. All the scores are guaranteed to be unique.
+// The athletes are placed based on their scores, where the 1st place athlete has the highest score, the 2nd place athlete has the 2nd highest score, and so on. The placement of each athlete determines their rank:
+// The 1st place athlete's rank is "Gold Medal".
+// The 2nd place athlete's rank is "Silver Medal".
+// The 3rd place athlete's rank is "Bronze Medal".
+// For the 4th place to the nth place athlete, their rank is their placement number (i.e., the xth place athlete's rank is "x").
+// Return an array answer of size n where answer[i] is the rank of the ith athlete.
+
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+		int n = score.size();
+		priority_queue< pair<int, int> > rankPQ;
+		vector<string> relativeRanks(n, "");
+		for(int i = 0; i < n; i++) rankPQ.push(make_pair(score[i], i));
+		int rank = 1;
+		while(!rankPQ.empty()) {
+			pair<int, int> curPair = rankPQ.top();
+			int scr = curPair.first;
+			int idx = curPair.second;
+			if(rank > 3) relativeRanks[idx] = to_string(rank);
+			else if(rank == 3) relativeRanks[idx] = "Bronze Medal";
+			else if(rank == 2) relativeRanks[idx] = "Silver Medal";
+			else relativeRanks[idx] = "Gold Medal";
+			rank++; 
+			rankPQ.pop();
+		}
+		return relativeRanks;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	int n; cin >> n;
+	vi score(n, 0);
+	for(int i = 0; i < n; i++) cin >> score[i];
+	Solution *soln = new Solution();
+	vs relativeRanks = soln->findRelativeRanks(score);
+	for(auto rr : relativeRanks) cout << rr << " ";
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
