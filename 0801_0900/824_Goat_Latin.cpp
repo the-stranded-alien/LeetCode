5f0e7a9fb877f3e392472ce650f7b96b3157747a
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2022-01-26 22:24:05 +0530

    LeetCode Practice

---
+++ 0801_0900/824_Goat_Latin.cpp
@@ -0,0 +1,134 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// You are given a string sentence that consist of words separated by spaces. Each word consists of lowercase and uppercase letters only.
+// We would like to convert the sentence to "Goat Latin" (a made-up language similar to Pig Latin.) The rules of Goat Latin are as follows:
+// If a word begins with a vowel ('a', 'e', 'i', 'o', or 'u'), append "ma" to the end of the word.
+// For example, the word "apple" becomes "applema".
+// If a word begins with a consonant (i.e., not a vowel), remove the first letter and append it to the end, then add "ma".
+// For example, the word "goat" becomes "oatgma".
+// Add one letter 'a' to the end of each word per its word index in the sentence, starting with 1.
+// For example, the first word gets "a" added to the end, the second word gets "aa" added to the end, and so on.
+// Return the final sentence representing the conversion from sentence to Goat Latin.
+
+class Solution {
+public:
+	bool isVowel(char ch) {
+		if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') return true;
+		else if(ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') return true;
+		else return false;
+	}
+
+    string toGoatLatin(string sentence) {
+        string goatLatin = "";
+        stringstream ss(sentence);
+        vector<string> tokens;
+        string token;
+        while(getline(ss, token, ' ')) {
+        	if((token.length() >= 1) && (token != " ")) tokens.push_back(token);
+        }
+        int aItr = 1;
+		for(int itr = 0; itr < tokens.size(); itr++) {
+			string cur = tokens[itr];
+			char firstChar = cur[0];
+			string newCur = "";
+			if(isVowel(firstChar)) {
+				newCur += cur;
+				newCur += "ma";
+			} else {
+				newCur += cur.substr(1);
+				newCur += cur[0];
+				newCur += "ma";
+			}
+			newCur.append(aItr, 'a');
+			aItr++;
+			goatLatin += newCur;
+			goatLatin += " ";
+		}		 
+		goatLatin.pop_back();
+        return goatLatin;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	string sentence;
+	getline(cin, sentence);
+	Solution *soln = new Solution();
+	string goat = soln->toGoatLatin(sentence);
+	cout << goat << nl;
+
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
