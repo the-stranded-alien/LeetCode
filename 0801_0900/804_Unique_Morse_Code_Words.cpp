35c08d7f5b3aeed23db3a4b49ef713f472e7f7f3
Sahil Gupta <thestrandedalien.mysticdark@gmail.com>
2022-02-01 13:10:46 +0530

    LeetCode Practice

---
+++ 0801_0900/804_Unique_Morse_Code_Words.cpp
@@ -0,0 +1,112 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+// 'a' maps to ".-",
+// 'b' maps to "-...",
+// 'c' maps to "-.-.", and so on.
+// For convenience, the full table for the 26 letters of the English alphabet is given below:
+// [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+// Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+// For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a concatenation the transformation of a word.
+// Return the number of different transformations among all words we have.
+
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+     	vector<string> morseMappings = {".-", "-...", "-.-.", "-..", ".", "..-.",
+     		 "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.",
+     		 "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+     	set<string> morseSet;
+     	for(auto word : words) {
+     		string curMorse = "";
+     		for(auto ch : word) curMorse += morseMappings[ch - '0' - 49];
+     		morseSet.insert(curMorse);
+     	}
+     	return morseSet.size();
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	int n; cin >> n;
+	vector<string> words(n, " ");
+	for(int i = 0; i < n; i++) cin >> words[i];
+	Solution *soln = new Solution();
+	int uniqueMorse = soln->uniqueMorseRepresentations(words);
+	cout << uniqueMorse << nl;
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
