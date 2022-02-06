039ce836ee1112286de5cb17feddebfdcc96509f
Sahil Gupta <thestrandedalien.mysticdark@gmail.com>
2022-02-06 22:33:28 +0530

    LeetCode Practice

---
+++ 2101_2200/2126_Destroying_Asteroids.cpp
@@ -0,0 +1,110 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// You are given an integer mass, which represents the original mass of a
+// planet. You are further given an integer array asteroids, where
+// asteroids[i] is the mass of the ith asteroid.
+// You can arrange for the planet to collide with the asteroids in any
+// arbitrary order. If the mass of the planet is greater than or equal
+// to the mass of the asteroid, the asteroid is destroyed and the planet
+// gains the mass of the asteroid. Otherwise, the planet is destroyed.
+// Return true if all asteroids can be destroyed. Otherwise, return false.
+
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        sort(asteroids.begin(), asteroids.end());
+        // long long int32_t mass;
+        for(int i = 0; i < asteroids.size(); i++) {
+        	if(mass < asteroids[i]) return false;
+        	else mass += asteroids[i];
+        }
+        return true;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	int mass; cin >> mass;
+	int ast; cin >> ast;
+	vi asteroids(ast, 0);
+	for(int i = 0; i < ast; i++) cin >> asteroids[i];
+	Solution *soln = new Solution();
+	bool canDestroy = soln->asteroidsDestroyed(mass, asteroids);
+	if(canDestroy) cout << "true" << nl;
+	else cout << "false" << nl;
+	return;	
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
