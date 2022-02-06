94b4ff7a91df8f78dbf991f44c9ad62ac92993ce
Sahil Gupta <thestrandedalien.mysticdark@gmail.com>
2022-02-06 21:52:17 +0530

    LeetCode Practice

---
+++ 0001_0100/080_Remove_Duplicates_from_Sorted_Array_2.cpp
@@ -0,0 +1,122 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// Given an integer array nums sorted in non-decreasing order, remove some
+// duplicates in-place such that each unique element appears at most twice.
+// The relative order of the elements should be kept the same.
+// Since it is impossible to change the length of the array in some languages,
+// you must instead have the result be placed in the first part of the array
+// nums. More formally, if there are k elements after removing the duplicates,
+// then the first k elements of nums should hold the final result. It does not
+// matter what you leave beyond the first k elements.
+// Return k after placing the final result in the first k slots of nums.
+// Do not allocate extra space for another array. You must do this by
+// modifying the input array in-place with O(1) extra memory.
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+    	int n = nums.size();
+    	if(n <= 2) return n;
+    	int itr = 0;
+    	for(int i = 2; i < n; i++) {
+    		if((nums[i] == nums[i - 1]) && (nums[i] == nums[i - 2])) {
+    			int c = nums[i];
+    			nums[i] = INT_MAX;
+    			while(((i + 1) < n) && (nums[i + 1] == c)) {
+    				nums[i + 1] = INT_MAX;
+    				i++; 
+    			}
+    		}
+    	}
+    	for(int i = 0; i < n; i++) {
+    		if(nums[i] != INT_MAX) nums[itr++] = nums[i]; 
+    	}
+    	return itr;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	int n; cin >> n;
+	vi nums(n, 0);
+	for(int i = 0; i < n; i++) cin >> nums[i];
+	Solution *soln = new Solution();
+	int k = soln->removeDuplicates(nums);
+	cout << k << nl;
+	for(int i = 0; i < k; i++) cout << nums[i] << " ";
+	return;	
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
