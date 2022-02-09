2d0dafed8abdcadbf34daee2969aa4e06df9d483
Sahil Gupta <thestrandedalien.mysticdark@gmail.com>
2022-02-09 18:37:13 +0530

    LeetCode Practice

---
+++ 2101_2200/2160_Minimum_Sum_of_Four_Digit_Number_After_Splitting_Digits.cpp
@@ -0,0 +1,110 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// You are given a positive integer num consisting of exactly four digits.
+// Split num into two new integers new1 and new2 by using the digits found
+// in num. Leading zeros are allowed in new1 and new2, and all the digits
+// found in num must be used.
+// For example, given num = 2932, you have the following digits: two 2's,
+// one 9 and one 3. Some of the possible pairs [new1, new2] are [22, 93],
+// [23, 92], [223, 9] and [2, 329].
+// Return the minimum possible sum of new1 and new2.
+
+class Solution {
+public:
+    int minimumSum(int num) {
+        vector<int> digits;
+        digits.reserve(4);
+        while(num > 0) {
+        	int dig = num % 10;
+        	digits.push_back(dig);
+        	num = num / 10;
+        }
+        sort(digits.begin(), digits.end());
+        int num1 = (10 * digits[0]) + digits[2];
+        int num2 = (10 * digits[1]) + digits[3];
+        return (num1 + num2);
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	int num; cin >> num;
+	Solution *soln = new Solution();
+	int ms = soln->minimumSum(num);
+	cout << ms << nl;
+	return;	
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
