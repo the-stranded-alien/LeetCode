51b97e3f10d6b0ebe2b340584fc5d1a9081783ca
Sahil Gupta <thestrandedalien.mysticdark@gmail.com>
2022-02-23 18:19:58 +0530

    LeetCode Practice

---
+++ 1801_1900/1812_Determine_Color_of_a_Chessboard_Square.cpp
@@ -0,0 +1,108 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// You are given coordinates, a string that represents the coordinates
+// of a square of the chessboard. Below is a chessboard for your
+// reference.
+// Return true if the square is white, and false if the square is black.
+// The coordinate will always represent a valid chessboard square.
+// The coordinate will always have the letter first, and the number
+// second.
+
+class Solution {
+public:
+    bool squareIsWhite(string coordinates) {
+        char c1 = coordinates[0];
+        char c2 = coordinates[1];
+        if(c1 == 'a' || c1 == 'c' || c1 == 'e' || c1 == 'g') {
+        	if(c2 == '1' || c2 == '3' || c2 == '5' || c2 == '7') return false;
+        	else return true;
+        } else {
+        	if(c2 == '1' || c2 == '3' || c2 == '5' || c2 == '7') return true;
+        	else return false;
+        }
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	string coordinates; cin >> coordinates;
+	Solution *soln = new Solution();
+	bool sqIsWhite = soln->squareIsWhite(coordinates);
+	if(sqIsWhite) cout << "true" << nl;
+	else cout << "false" << nl;
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
