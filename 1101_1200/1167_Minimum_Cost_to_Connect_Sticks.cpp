ddc06b60741ee4b326d7dd3b89579cf7a000ba36
Sahil Gupta <thestrandedalien.mysticdark@gmail.com>
2022-02-28 22:43:04 +0530

    LeetCode Practice

---
+++ 1101_1200/1167_Minimum_Cost_to_Connect_Sticks.cpp
@@ -0,0 +1,109 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// You have some number of sticks with positive integer lengths. These lengths are given as an
+// array sticks, where sticks[i] is the length of the ith stick. You can connect any two sticks
+// of lengths x and y into one stick by paying a cost of x + y. You must connect all the sticks
+// until there is only one stick remaining. Return the minimum cost of connecting all the given
+// sticks into one stick in this way.
+
+class Solution {
+public:
+    int connectSticks(vector<int>& sticks) {
+        priority_queue<int, vector<int>, greater<int> > minHeap(sticks.begin(), sticks.end());
+        int minCost = 0;
+        while(minHeap.size() > 1) {
+        	int min1 = minHeap.top();
+        	minHeap.pop();
+        	int min2 = minHeap.top();
+        	minHeap.pop();
+        	minCost += (min1 + min2);
+        	minHeap.push(min1 + min2);
+        }
+        return minCost;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	int s; cin >> s;
+	vector<int> sticks(s, 0);
+	for(int i = 0; i < s; i++) cin >> sticks[i];
+	Solution *soln = new Solution();
+	int minCost = soln->connectSticks(sticks);
+	cout << minCost << nl;
+	return;	
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
