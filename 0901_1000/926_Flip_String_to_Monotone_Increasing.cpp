dfd204d7cac25721567f8f337a333b6fce923573
Sahil Gupta <thestrandedalien.mysticdark@gmail.com>
2022-02-28 13:21:30 +0530

    LeetCode Practice

---
+++ 0901_1000/926_Flip_String_to_Monotone_Increasing.cpp
@@ -0,0 +1,105 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// A binary string is monotone increasing if it consists of some number of 0's
+// (possibly none), followed by some number of 1's (also possibly none).
+// You are given a binary string s. You can flip s[i] changing it from 0
+// to 1 or from 1 to 0.
+// Return the minimum number of flips to make s monotone increasing.
+
+class Solution {
+public:
+    int minFlipsMonoIncr(string s) {
+        int l = s.length();
+        vector<int> prefix((l + 1), 0);
+        for(int i = 0; i < l; i++) {
+        	prefix[i + 1] = prefix[i] + (s[i] == '1' ? 1 : 0);
+        }
+        int minFlips = INT_MAX;
+        for(int j = 0; j <= l; j++) {
+        	minFlips = min(minFlips, (prefix[j] + l - j - (prefix[l] - prefix[j])));
+        }
+        return minFlips;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	string s; cin >> s;
+	Solution *soln = new Solution();
+	cout << soln->minFlipsMonoIncr(s) << nl;
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
