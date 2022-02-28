27038e27fd3b3bc5cfde7f667757959a5ea12538
Sahil Gupta <thestrandedalien.mysticdark@gmail.com>
2022-02-28 23:42:16 +0530

    LeetCode Practice

---
+++ 0201_0300/252_Meeting_Rooms.cpp
@@ -0,0 +1,108 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// Given an array of meeting time intervals where intervals[i] = [starti, endi],
+// determine if a person could attend all meetings.
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+		int sz = intervals.size();
+        if(sz <= 1) return true;
+        sort(intervals.begin(), intervals.end());
+		int start = intervals[0][0];
+		int end = intervals[0][1];
+		for(int i = 1; i < sz; i++) {
+			int nextS = intervals[i][0];
+			int nextE = intervals[i][1];
+			if(end > nextS) return false;
+			start = nextS;
+			end = nextE;
+		}
+		return true;
+    }
+};
+
+void solve() {
+	int in; cin >> in;
+	vector< vector<int> > intervals(in, vector<int>(2, 0));
+	for(int i = 0; i < in; i++) cin >> intervals[i][0] >> intervals[i][1];
+	Solution *soln = new Solution();
+	bool canAttend = soln->canAttendMeetings(intervals);
+	if(canAttend) cout << "True" << nl;
+	else cout << "False" << nl;
+	return;		
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
