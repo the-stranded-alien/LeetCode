2a712630b3a8668cd250ffb8b1d67c9ad5e7fb11
Sahil Gupta <thestrandedalien.mysticdark@gmail.com>
2022-03-09 21:00:38 +0530

    LeetCode Practice

---
+++ 0001_0100/056_Merge_Intervals.cpp
@@ -0,0 +1,112 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// Given an array of intervals where intervals[i] = [starti, endi], merge all
+// overlapping intervals, and return an array of the non-overlapping intervals
+// that cover all the intervals in the input.
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    	if(intervals.size() == 1) return intervals;
+        sort(intervals.begin(), intervals.end());
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        vector< vector<int> > mer;
+        for(int i = 1; i < intervals.size(); i++) {
+        	if(end >= intervals[i][0]) {
+        		end = max(end, intervals[i][1]);
+        	} else {
+        		mer.push_back({start, end});
+        		start = intervals[i][0];
+        		end = intervals[i][1];
+        	}
+        }
+        mer.push_back({start, end});
+        return mer;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	int in; cin >> in;
+	vector< vector<int> > intervals(in, vector<int>(2, 0));
+	for(int i = 0; i < in; i++) cin >> intervals[i][0] >> intervals[i][1];
+	Solution *soln = new Solution();
+	vvi merge = soln->merge(intervals);
+	for(auto inter : merge) cout << inter[0] << " " << inter[1] << nl;
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
