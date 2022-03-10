28bd8e88e745d4d0a88fa9c354dbfa430a29c4df
Sahil Gupta <thestrandedalien.mysticdark@gmail.com>
2022-03-10 22:32:55 +0530

    LeetCode Practice

---
+++ 0101_0200/162_Find_Peak_Element.cpp
@@ -0,0 +1,106 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// A peak element is an element that is strictly greater than its neighbors.
+// Given an integer array nums, find a peak element, and return its index.
+// If the array contains multiple peaks, return the index to any of the peaks.
+// You may imagine that nums[-1] = nums[n] = -∞.
+// You must write an algorithm that runs in O(log n) time.
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while(left < right) {
+        	int mid = (left + right) / 2;
+        	if(nums[mid] > nums[mid + 1]) right = mid;
+        	else left = mid + 1;
+        }
+        return left;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	int n; cin >> n;
+	vector<int> nums(n, 0);
+	for(int i = 0; i < n; i++) cin >> nums[i];
+	Solution *soln = new Solution();	
+	int peak = soln->findPeakElement(nums);
+	cout << peak << nl;
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
