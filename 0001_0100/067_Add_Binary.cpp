0d75da0975914a5ba58178d65d86ed2a3596e840
Sahil Gupta <thestrandedalien.mysticdark@gmail.com>
2022-03-12 19:33:08 +0530

    LeetCode Practice

---
+++ 0001_0100/067_Add_Binary.cpp
@@ -0,0 +1,113 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// Given two binary strings a and b, return their sum as a binary string.
+
+class Solution {
+public:
+    string addBinary(string a, string b) {
+    	int carry = 0;
+    	string res = "";
+    	reverse(a.begin(), a.end());
+    	reverse(b.begin(), b.end());
+    	int itr = 0;
+    	int sum;
+    	int al = a.length();
+    	int bl = b.length();
+    	while((itr < al) || (itr < bl)) {
+    		sum = carry;
+    		if(itr < al) sum += a[itr] - '0';
+    		if(itr < bl) sum += b[itr] - '0';
+    		if(sum == 0) carry = 0, res += '0';
+    		else if(sum == 1) carry = 0, res += '1';
+    		else if(sum == 2) carry = 1, res += '0';
+    		else carry = 1, res += '1';
+    		itr++;
+    	}
+    	if(carry == 1) res += '1';
+    	reverse(res.begin(), res.end());
+    	return res;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	string a; cin >> a;
+	string b; cin >> b;
+	Solution *soln = new Solution();
+	cout << soln->addBinary(a, b) << nl;
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
