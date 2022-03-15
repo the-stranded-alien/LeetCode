3c5763f9f0714a6cc1ccc3a9e9a54721ab001696
Sahil Gupta <thestrandedalien.mysticdark@gmail.com>
2022-03-15 22:13:32 +0530

    LeetCode Practice

---
+++ 1501_1600/1502_Can_Make_Arithmetic_Progression_From_Sequence.cpp
@@ -0,0 +1,108 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// A sequence of numbers is called an arithmetic progression if the difference
+// between any two consecutive elements is the same. Given an array of numbers
+// arr, return true if the array can be rearranged to form an arithmetic progression. Otherwise, return false.
+
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        if(arr.size() == 2) return true;
+        sort(arr.begin(), arr.end());
+        int a1 = arr[0];
+        int a2 = arr[1];
+        int diff = a2 - a1;
+        for(int i = 1; i < (arr.size() - 1); i++) {
+        	a1 = arr[i];
+        	a2 = arr[i + 1];
+        	if(diff != (a2 - a1)) return false;
+        }
+        return true;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	int a; cin >> a;
+	vector<int> arr(a, 0);
+	for(int i = 0; i < a; i++) cin >> arr[i];
+	Solution *soln = new Solution();
+	bool ap = soln->canMakeArithmeticProgression(arr);
+	if(ap) cout << "true" << nl;
+	else cout << "false" << nl;
+	return;	
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
