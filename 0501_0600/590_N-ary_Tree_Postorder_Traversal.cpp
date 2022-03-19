2f6499e0cd8645c8bb2cb6d4c90c4fdafcd62fb8
Sahil Gupta <thestrandedalien.mysticdark@gmail.com>
2022-03-19 18:57:57 +0530

    LeetCode Practice

---
+++ 0501_0600/590_N-ary_Tree_Postorder_Traversal.cpp
@@ -0,0 +1,117 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// Given the root of an n-ary tree, return the postorder traversal of its
+// nodes' values. Nary-Tree input serialization is represented in their
+// level order traversal. Each group of children is separated by the null value.
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> postorder(Node* root) {
+        if(root == nullptr) return {};
+        stack<Node*> stk;
+        vector<int> post;
+        stk.push(root);
+        while(!stk.empty()) {
+        	Node *cur = stk.top();
+        	stk.pop();
+        	post.push_back(cur->val);
+        	for(auto child : cur->children) stk.push(child);
+        }
+        reverse(post.begin(), post.end());
+    	return post;
+    }
+};
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
