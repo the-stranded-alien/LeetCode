82febd08c1bcdc6c7c80617c097bde61330f43bd
Sahil Gupta <thestrandedalien.mysticdark@gmail.com>
2022-03-19 15:52:50 +0530

    LeetCode Practice

---
+++ 1301_1400/1309_Decrypt_String_from_Alphabet_to_Integer_Mapping.cpp
@@ -0,0 +1,115 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// You are given a string s formed by digits and '#'. We want to
+// map s to English lowercase characters as follows:
+// Characters ('a' to 'i') are represented by ('1' to '9') respectively.
+// Characters ('j' to 'z') are represented by ('10#' to '26#') respectively.
+// Return the string formed after mapping.
+// The test cases are generated so that a unique mapping will always exist.
+
+class Solution {
+public:
+    string freqAlphabets(string s) {
+		int l = (s.length() - 1);
+		unordered_map<int, char> um;
+		for(int i = 0; i < 26; i++) um[i + 1] = ('a' + i); 
+		string res = "";
+		while(l >= 0) {
+			if(s[l] == '#') {
+				int c2 = s[l - 1] - '0';
+				int c1 = s[l - 2] - '0';
+				int cur = ((10 * c1) + c2);
+				res.push_back(um[cur]);
+				l -= 3;
+			} else {
+				int cur = s[l--] - '0';
+				res.push_back(um[cur]);
+			}
+		}
+		reverse(res.begin(), res.end());
+		return res;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	string s; cin >> s;
+	Solution *soln = new Solution();
+	string res = soln->freqAlphabets(s);
+	cout << res << nl;
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
