f6fd106d314a33b5a4ffc244d98bd7293ea052e9
Sahil Gupta <thestrandedalien.mysticdark@gmail.com>
2022-03-19 12:35:15 +0530

    LeetCode Practice

---
+++ 1701_1800/1768_Merge_Strings_Alternately.cpp
@@ -0,0 +1,104 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// You are given two strings word1 and word2. Merge the strings by adding
+// letters in alternating order, starting with word1. If a string is longer
+// than the other, append the additional letters onto the end of the merged
+// string. Return the merged string.
+
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int itr1 = 0;
+        int itr2 = 0;
+        string merged = "";
+        while((itr1 < word1.length()) || (itr2 < word2.length())) {
+        	if(itr1 < word1.length()) merged += word1[itr1++];
+        	if(itr2 < word2.length()) merged += word2[itr2++]; 
+        }
+        return merged;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	string word1; cin >> word1;
+	string word2; cin >> word2;
+	Solution *soln = new Solution();
+	string merged = soln->mergeAlternately(word1, word2);
+	cout << merged << nl;
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
