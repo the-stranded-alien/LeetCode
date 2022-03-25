a999d3862890c98cff1b8801bdcb0c73fb388210
Sahil Gupta <thestrandedalien.mysticdark@gmail.com>
2022-03-25 20:30:34 +0530

    LeetCode Practice

---
+++ 0901_1000/931_Minimum_Falling_Path_Sum.cpp
@@ -0,0 +1,117 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// Given an n x n array of integers matrix, return the minimum sum of any falling
+// path through matrix. A falling path starts at any element in the first row and
+// chooses the element in the next row that is either directly below or diagonally
+// left/right. Specifically, the next element from position (row, col) will be
+// (row + 1, col - 1), (row + 1, col), or (row + 1, col + 1).
+
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector< vector<int> > dp(m, vector<int>(n));
+        for(int i = 0; i < n; i++) dp[0][i] = matrix[0][i];
+        for(int i = 1; i < m; i++) {
+        	for(int j = 0; j < n; j++) {
+        		if(j == 0) dp[i][j] = matrix[i][j] + min(dp[i - 1][j], dp[i - 1][j + 1]);
+        		else if(j == (m - 1)) dp[i][j] = matrix[i][j] + min(dp[i - 1][j], dp[i - 1][j - 1]);
+        		else dp[i][j] = matrix[i][j] + min(dp[i - 1][j], min(dp[i - 1][j - 1], dp[i - 1][j + 1]));
+        	}
+        }	
+        int minSum = dp[m - 1][0];
+        for(int i = 0 ; i < n; i++){
+            minSum = min(minSum, dp[m - 1][i]);
+        }
+        return minSum;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	int m; cin >> m;
+	int n; cin >> n;
+	vector< vector<int> > matrix(m, vector<int>(n, 0));
+	for(int i = 0; i < m; i++) {
+		for(int j = 0; j < n; j++) cin >> matrix[i][j];
+	}	
+	Solution *soln = new Solution();
+	int minSum = soln->minFallingPathSum(matrix);
+	cout << minSum << nl;
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
