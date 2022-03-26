3430aa38f459c979f464f93c12995b388a122a17
Sahil Gupta <thestrandedalien.mysticdark@gmail.com>
2022-03-26 20:04:15 +0530

    LeetCode Practice

---
+++ 2201_2300/2214_Minimum_Health_to_Beat_Game.cpp
@@ -0,0 +1,102 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// You are playing a game that has n levels numbered from 0 to n - 1. You are
+// given a 0-indexed integer array damage where damage[i] is the amount of health
+// you will lose to complete the ith level. You are also given an integer armor.
+// You may use your armor ability at most once during the game on any level which
+// will protect you from at most armor damage. You must complete the levels in
+// order and your health must be greater than 0 at all times to beat the game.
+// Return the minimum health you need to start with to beat the game.
+
+class Solution {
+public:
+    long long minimumHealth(vector<int>& damage, int armor) {
+    	return (accumulate(damage.begin(), damage.end(), 0LL) -
+            min(*max_element(damage.begin(), damage.end()), armor) + 1);
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	int d; cin >> d;
+	vector<int> damage(d, 0);
+	for(int i = 0; i < d; i++) cin >> damage[i];
+	int armor; cin >> armor;
+	Solution *soln = new Solution();
+	cout << soln->minimumHealth(damage, armor) << nl;
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
