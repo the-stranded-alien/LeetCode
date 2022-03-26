1bb451f9e7a277c5f46740906bc67e5836540098
Sahil Gupta <thestrandedalien.mysticdark@gmail.com>
2022-03-26 22:23:02 +0530

    LeetCode Practice

---
+++ 2101_2200/2125_Number_of_Laser_Beams_in_a_Bank.cpp
@@ -0,0 +1,113 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// Anti-theft security devices are activated inside a bank. You are given a 0-indexed binary
+// string array bank representing the floor plan of the bank, which is an m x n 2D matrix. bank[i] represents the ith row, consisting of '0's and '1's. '0' means the cell is empty, while'1' means the cell has a security device.
+// There is one laser beam between any two security devices if both conditions are met:
+// The two devices are located on two different rows: r1 and r2, where r1 < r2.
+// For each row i where r1 < i < r2, there are no security devices in the ith row.
+// Laser beams are independent, i.e., one beam does not interfere nor join with another.
+// Return the total number of laser beams in the bank.
+
+class Solution {
+public:
+	int lasers(string cur) {
+		int l = 0;
+		for(auto c : cur) if(c == '1') l++;
+		return l;
+	}
+
+    int numberOfBeams(vector<string>& bank) {
+        int prev = 0;
+        int beams = 0;
+        for(int i = 0; i < bank.size(); i++) {
+        	int cur = lasers(bank[i]);
+        	beams += (prev * cur);
+        	if(cur != 0) prev = cur;
+        }
+        return beams;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	int b; cin >> b;
+	vector<string> bank(b);
+	for(int i = 0; i < b; i++) cin >> bank[i];
+	Solution *soln = new Solution();
+	cout << soln->numberOfBeams(bank) << nl;
+	return;	
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
