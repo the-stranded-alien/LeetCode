dad42ae4602c8360ecf1062553cc073377f4a88f
Sahil Gupta <thestrandedalien.mysticdark@gmail.com>
2022-04-07 18:00:58 +0530

    LeetCode Practice

---
+++ 0301_0400/392_Is_Subsequence.cpp
@@ -0,0 +1,110 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+// A subsequence of a string is a new string that is formed from the original
+// string by deleting some (can be none) of the characters without disturbing
+// the relative positions of the remaining characters. (i.e., "ace" is a
+// subsequence of "abcde" while "aec" is not).
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int lenS = s.length();
+        int lenT = t.length();
+        int ti = 0;
+        for(int sitr = 0; sitr < lenS; sitr++) {
+        	bool isPresent = false;
+        	while(ti < lenT) {
+        		if(s[sitr] == t[ti]) { ti++; isPresent = true; break; }
+        		else ti++;
+        	}
+        	if(!isPresent) return false;
+        }
+        return true;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	string s; cin >> s;
+	string t; cin >> t;
+	Solution *soln = new Solution();
+	bool is = soln->isSubsequence(s, t);
+	if(is) cout << "true\n";
+	else cout << "false\n";
+	return;	
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
