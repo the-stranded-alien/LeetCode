5f3534dbc992cf70a3a61903914a66a91b5d1ad6
Sahil Gupta <thestrandedalien.mysticdark@gmail.com>
2022-04-07 13:13:38 +0530

    LeetCode Practice

---
+++ 1001_1100/1046_Last_Stone_Weight.cpp
@@ -0,0 +1,108 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// You are given an array of integers stones where stones[i] is the weight of the ith stone.
+// We are playing a game with the stones. On each turn, we choose the heaviest
+// two stones and smash them together. Suppose the heaviest two stones have weights x and y with x <= y. The result of this smash is:
+// If x == y, both stones are destroyed, and
+// If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+// At the end of the game, there is at most one stone left.
+// Return the smallest possible weight of the left stone. If there are no stones left, return 0.
+
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> maxHeap(stones.begin(), stones.end());
+        while(maxHeap.size() >= 2) {
+        	int s1 = maxHeap.top();
+        	maxHeap.pop();
+        	int s2 = maxHeap.top();
+        	maxHeap.pop();
+        	if(s1 != s2) maxHeap.push(abs(s1 - s2));
+        }
+        return (maxHeap.size() == 0 ? 0 : maxHeap.top());
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	int s; cin >> s;
+	vector<int> stones(s, 0);
+	for(int i = 0; i < s; i ++) cin >> stones[i];
+	Solution *soln = new Solution();
+	cout << soln->lastStoneWeight(stones) << nl;
+	return;	
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
