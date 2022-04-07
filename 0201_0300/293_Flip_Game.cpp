02d92637066acf50cc5271e298344e38a74202d4
Sahil Gupta <thestrandedalien.mysticdark@gmail.com>
2022-04-07 16:40:46 +0530

    LeetCode Practice

---
+++ 0201_0300/293_Flip_Game.cpp
@@ -0,0 +1,110 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// You are playing a Flip Game with your friend.
+
+// You are given a string currentState that contains only '+' and '-'. You and
+// your friend take turns to flip two consecutive "++" into "--". The game ends
+// when a person can no longer make a move, and therefore the other person will
+// be the winner. Return all possible states of the string currentState after
+// one valid move. You may return the answer in any order. If there is no valid
+// move, return an empty list [].
+
+class Solution {
+public:
+    vector<string> generatePossibleNextMoves(string currentState) {
+    	if(currentState.length() == 1) return {};
+    	vector<string> nextMoves;
+    	for(int i = 0; i < currentState.length() - 1; i++) {
+    		if(currentState[i] == '+' && currentState[i + 1] == '+') {
+    			string nextMove = currentState;
+    			nextMove[i] = '-';
+    			nextMove[i + 1] = '-';
+    			nextMoves.push_back(nextMove);
+    		}
+    	}
+    	return nextMoves;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	string cs; cin >> cs;
+	Solution *soln = new Solution();
+	vs nm = soln->generatePossibleNextMoves(cs);
+	for(auto next : nm) cout << next << nl;
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
