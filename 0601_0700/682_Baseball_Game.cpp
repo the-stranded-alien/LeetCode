c62b7283afce1d921dadefe863a2914a6258bd0a
Sahil Gupta <thestrandedalien.mysticdark@gmail.com>
2022-04-10 21:26:59 +0530

    LeetCode Practice

---
+++ 0601_0700/682_Baseball_Game.cpp
@@ -0,0 +1,123 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// You are keeping score for a baseball game with strange rules. The game consists of several rounds, where the scores of past rounds may affect future rounds' scores.
+
+// At the beginning of the game, you start with an empty record. You are given a list of strings ops, where ops[i] is the ith operation you must apply to the record and is one of the following:
+
+// An integer x - Record a new score of x.
+// "+" - Record a new score that is the sum of the previous two scores. It is guaranteed there will always be two previous scores.
+// "D" - Record a new score that is double the previous score. It is guaranteed there will always be a previous score.
+// "C" - Invalidate the previous score, removing it from the record. It is guaranteed there will always be a previous score.
+// Return the sum of all the scores on the record.
+
+class Solution {
+public:
+    int calPoints(vector<string>& ops) {
+        stack<int> numStack;
+        int score = 0;
+        for(auto op : ops) {
+        	if(op == "C") numStack.pop();
+        	else if(op == "D") {
+        		int tp = numStack.top();
+        		numStack.push(tp * 2);
+        	} else if(op == "+") {
+        		int tp1 = numStack.top();
+        		numStack.pop();
+        		int tp2 = numStack.top();
+        		numStack.pop();
+        		numStack.push(tp2);
+        		numStack.push(tp1);
+        		numStack.push(tp1 + tp2);
+        	} else numStack.push(stoi(op));
+        }
+        while(!numStack.empty()) {
+        	score += numStack.top();
+        	numStack.pop();
+        }
+        return score;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	int s; cin >> s;
+	vs ops(s, " ");
+	for(int i = 0; i < s; i++) cin >> ops[i];
+	Solution *soln = new Solution();
+	cout << soln->calPoints(ops) << nl;
+	return;	
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
