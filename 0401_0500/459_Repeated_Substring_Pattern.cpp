10ddf34df8ec27f23d568af16347514163729c23
Sahil Gupta <thestrandedalien.mysticdark@gmail.com>
2022-04-10 20:57:09 +0530

    LeetCode Practice

---
+++ 0401_0500/459_Repeated_Substring_Pattern.cpp
@@ -0,0 +1,114 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// Given a string s, check if it can be constructed by taking a substring of it
+// and appending multiple copies of the substring together.
+
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+    	for(int sz = 1; sz < (s.length() / 2) + 1; sz++) {
+    		string sub = s.substr(0, sz);
+    		int sbsz = sub.length();
+    		bool isRep = true;
+    		for(int itr = 0; itr < s.length(); itr += sbsz) {
+    			string x = s.substr(itr, sbsz);
+    			if(sub != x) {
+    				isRep = false;
+    				break;
+    			}
+    		}
+    		if(isRep) return true;
+    	}
+    	return false;
+    }
+    bool repeatedSubstringPatternOpt(string s) {
+        string doubled = s + s;
+        int cut = doubled.find(s, 1);
+        if (cut >= 1 && cut < s.length()) return true;
+        return false;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	string s; cin >> s;
+	Solution *soln = new Solution();
+	bool pos = soln->repeatedSubstringPattern(s);
+	if(pos) cout << "true\n";
+	else cout << "false\n";
+	return;	
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
