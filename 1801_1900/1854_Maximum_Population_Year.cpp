53d059c876e7483a4f97ebf7783b609b7394057c
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-11-03 20:16:54 +0530

    LeetCode Practice

---
+++ 1801_1900/1854_Maximum_Population_Year.cpp
@@ -0,0 +1,120 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// You are given a 2D integer array logs where each logs[i] = [birthi, deathi] indicates the birth and death years of the ith person.
+
+// The population of some year x is the number of people alive during that year. The ith person is counted in year x's population if x is in the inclusive range [birthi, deathi - 1]. Note that the person is not counted in the year that they die.
+
+// Return the earliest year with the maximum population.
+
+class Solution {
+public:
+	int maximumPopulation(vector<vector<int>>& logs) {
+        const int s = 1950;
+        if(logs.size() == 1) return logs[0][0];
+        vector<int> alive(101, 0);
+        for(vector<int> log : logs) {
+        	int y1 = log[0];
+        	int y2 = log[1];
+        	for(int i = y1; i < y2; i++) {
+        		alive[i - s] += 1;
+        	}
+        }
+        int maxV = INT_MIN;
+        int maxY = 0;
+        int idx = 0;
+        for(int a : alive) {
+        	if(a > maxV) {
+        		maxV = a;
+        		maxY = idx + s;
+        	}
+        	idx += 1;
+        }
+        return maxY;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	
+	int n; cin >> n;
+	vvi logs(n, vector<int>(2, 0));
+	for(int i = 0; i < n; i++) cin >> logs[i][0] >> logs[i][1];
+	Solution *soln = new Solution();
+	cout << soln->maximumPopulation(logs);
+
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
