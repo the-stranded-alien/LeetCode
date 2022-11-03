a9966e20e10edf656285d460ca10ede2db90f466
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-11-03 20:00:16 +0530

    LeetCode Practice

---
+++ 2101_2200/2131_Longest_Palindrome_by_Concatenating_Two_Letter_Words.cpp
@@ -0,0 +1,145 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// You are given an array of strings words. Each element of words consists of two lowercase English letters.
+
+// Create the longest possible palindrome by selecting some elements from words and concatenating them in any order. Each element can be selected at most once.
+
+// Return the length of the longest palindrome that you can create. If it is impossible to create any palindrome, return 0.
+
+// A palindrome is a string that reads the same forward and backward.
+
+class Solution {
+public:
+	int longestPalindrome(vector<string>& words) {
+        unordered_map<string, int> freqMap;
+        for(auto word : words) {
+        	if(freqMap.find(word) != freqMap.end()) freqMap[word] += 1;
+        	else freqMap[word] = 1;
+        }
+        int l = 0;
+        for(auto fr : freqMap) {
+        	string s = fr.first;
+        	int v = fr.second;
+        	string r = s;
+        	reverse(r.begin(), r.end());
+        	if(s == r) {
+        		int c = freqMap[s];
+        		if(c % 2 == 0) {
+        			l += 2 * c;
+        			freqMap.erase(s);
+        		} else {
+        			l += 2 * (c - 1);
+        			freqMap[s] = 1;
+        		}
+        	} else {
+        		if(freqMap.find(r) != freqMap.end()) {
+        			int rv = freqMap[r];        		
+        			int m = min(v, rv);
+        			l += 2 * (2 * m);
+        			freqMap[s] -= m;
+        			freqMap[r] -= m;
+
+        		}
+        	}
+        }
+        for(auto fr : freqMap) {
+        	string s = fr.first;
+        	int v = fr.second;
+        	string r = s;
+        	reverse(r.begin(), r.end());
+        	if(s == r && v > 0) {
+        		l += 2;
+        		break;
+        	}
+        }
+        return l;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	
+	int n; cin >> n;
+	vs words(n, " ");
+	for(int i = 0; i < n; i++) {
+		cin >> words[i];
+	}
+	Solution *soln = new Solution();
+	cout << soln->longestPalindrome(words);
+
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "Run Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
