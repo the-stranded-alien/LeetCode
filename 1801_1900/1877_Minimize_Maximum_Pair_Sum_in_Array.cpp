3701b9ddb2861f77851aad98c13ddc3ffe05d6dc
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-11-26 16:48:21 +0530

    LeetCode Practice

---
+++ 1801_1900/1877_Minimize_Maximum_Pair_Sum_in_Array.cpp
@@ -0,0 +1,113 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// The pair sum of a pair (a,b) is equal to a + b. The maximum pair sum is the
+// largest pair sum in a list of pairs.
+// For example, if we have pairs (1,5), (2,3), and (4,4), the maximum pair
+// sum would be max(1+5, 2+3, 4+4) = max(6, 5, 8) = 8.
+// Given an array nums of even length n, pair up the elements of nums into
+// n / 2 pairs such that:
+// Each element of nums is in exactly one pair, and
+// The maximum pair sum is minimized.
+// Return the minimized maximum pair sum after optimally pairing up 
+// the elements.
+
+
+class Solution {
+public:
+	 int minPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int ps = 0;
+        int sz = nums.size();
+        for(int i = 0; i < sz / 2; i++) {
+        	int s = nums[i] + nums[sz - 1 - i];
+        	ps = max(ps, s);
+        }
+        return ps;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	int n; cin >> n;
+	vi nums(n);
+	for(int i = 0; i < n; i++) cin >> nums[i];
+	
+	Solution *soln = new Solution();
+	cout << soln->minPairSum(nums);
+
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
