de454a97352fe8ca48c3835ee0f18b81e5058e2b
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-12-01 17:27:26 +0530

    LeetCode Practice

---
+++ 0801_0900/853_Car_Fleet.cpp
@@ -0,0 +1,118 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// There are n cars going to the same destination along a one-lane road. The destination is target miles away.
+// You are given two integer array position and speed, both of length n, where position[i] is the position of the ith car and speed[i] is the speed of the ith car (in miles per hour).
+// A car can never pass another car ahead of it, but it can catch up to it and drive bumper to bumper at the same speed. The faster car will slow down to match the slower car's speed. The distance between these two cars is ignored (i.e., they are assumed to have the same position).
+// A car fleet is some non-empty set of cars driving at the same position and same speed. Note that a single car is also a car fleet.
+// If a car catches up to a car fleet right at the destination point, it will still be considered as one car fleet.
+// Return the number of car fleets that will arrive at the destination.
+
+
+class Solution {
+public:
+	int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        set<pair<float, float> > ps;
+        int n = position.size();
+        for(int i = 0; i < n; i++) ps.insert({-position[i], speed[i]});
+
+        auto carItr = ps.begin();
+    	float tr = -1;
+    	int fleets = 0;
+
+    	while(carItr != ps.end()) {
+    		float x = (target + carItr->first) * carItr->second;
+    		if(x > tr) fleets++, tr = x;
+    		carItr++;
+    	}
+        return fleets;
+    }	
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	int target; cin >> target;
+	int n; cin >> n;
+	vi position(n);
+	vi speed(n);
+	for(int i = 0; i < n; i++) cin >> position[i];
+	for(int i = 0; i < n; i++) cin >> speed[i];
+	
+	Solution *soln = new Solution();
+	cout << soln->carFleet(target, position, speed);
+
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
