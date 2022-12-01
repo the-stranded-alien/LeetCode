ff089113929c80fcb231856a544f3469da29429b
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-12-01 23:35:38 +0530

    LeetCode Practice

---
+++ 1901_2000/1935_Maximum_Number_of_Words_You_Can_Type.cpp
@@ -0,0 +1,120 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// There is a malfunctioning keyboard where some letter keys do not work.
+// All other keys on the keyboard work properly.
+// Given a string text of words separated by a single space
+// (no leading or trailing spaces) and a string brokenLetters
+// of all distinct letter keys that are broken, return the number
+// of words in text you can fully type using this keyboard.
+
+class Solution {
+public:
+	int canBeTypedWords(string text, string brokenLetters) {
+        vector<string> words;
+        string wordToken;
+        stringstream ss(text);
+        while(getline(ss, wordToken, ' ')) {
+        	if(wordToken != "") words.push_back(wordToken);
+        }
+        set<char> bls(brokenLetters.begin(), brokenLetters.end());
+
+       	int count = 0;
+       	for(auto word : words) {
+       		bool couldBeTyped = true;
+       		for(auto ch : word) {
+       			if(bls.find(ch) != bls.end()) {
+       				couldBeTyped = false;
+       				continue;
+       			}
+       		}
+       		if(couldBeTyped) count++;
+       	}
+       	return count;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	string text;
+	getline(cin, text);
+	string brokenLetters; cin >> brokenLetters;	
+	
+	Solution *soln = new Solution();
+	cout << soln->canBeTypedWords(text, brokenLetters);
+
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
