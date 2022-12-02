cbb291b37a910bed8314ee887bd4c7f6335d8280
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-12-02 23:54:53 +0530

    LeetCode Practice

---
+++ 2201_2300/2215_Find_the_Difference_of_Two_Arrays.cpp
@@ -0,0 +1,116 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
+// answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
+// answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
+// Note that the integers in the lists may be returned in any order.
+
+class Solution {
+public:
+	vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+        set<int> s1(nums1.begin(), nums1.end());
+        set<int> s2(nums2.begin(), nums2.end());
+
+        set<int> intersection;
+        set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(), inserter(intersection, intersection.begin()));
+
+        vector<int> v1;
+        set_difference(s1.begin(), s1.end(), intersection.begin(), intersection.end(), inserter(v1, v1.begin()));
+
+        vector<int> v2;
+        set_difference(s2.begin(), s2.end(), intersection.begin(), intersection.end(), inserter(v2, v2.begin()));
+    
+        return {v1, v2};
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	int n1; cin >> n1;
+	vi nums1(n1);
+	for(int i = 0; i < n1; i++) cin >> nums1[i];
+	int n2; cin >> n2;
+	vi nums2(n2);
+	for(int i = 0; i < n2; i++) cin >> nums2[i];
+	
+	Solution *soln = new Solution();
+	vvi diff = soln->findDifference(nums1, nums2);
+	for(auto a1 : diff[0]) cout << a1 << " ";
+	cout << nl;
+	for(auto a2 : diff[1]) cout << a2 << " ";
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
