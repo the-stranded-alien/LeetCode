e0d55079b79ff466c83decf326140c60e0475525
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-12-05 23:32:27 +0530

    LeetCode Practice

---
+++ 1901_2000/1974_Minimum_Time_to_Type_Word_Using_Special_Typewriter.cpp
@@ -0,0 +1,110 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// There is a special typewriter with lowercase English letters 'a' to 'z'
+// arranged in a circle with a pointer. A character can only be typed if the
+// pointer is pointing to that character. The pointer is initially pointing to
+// the character 'a'.
+// Each second, you may perform one of the following operations:
+// Move the pointer one character counterclockwise or clockwise.
+// Type the character the pointer is currently on.
+// Given a string word, return the minimum number of seconds to type
+// out the characters in word.
+
+class Solution {
+public:
+	int minTimeToType(string word) {
+        int time = word.length();
+        char prev = 'a';
+        for(const char &curr : word) {
+        	int curr_to_prev = abs(curr - prev);
+        	int prev_to_curr = 26 - abs(prev - curr);
+        	time += min(curr_to_prev, prev_to_curr);
+        	prev = curr;
+        }
+        return time;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	string word; cin >> word;
+	
+	Solution *soln = new Solution();
+	cout << soln->minTimeToType(word);
+
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
