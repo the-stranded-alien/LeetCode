53d20d7166675b71d35c23f4a2fd7a8567a9b36e
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-12-16 18:05:10 +0530

    LeetCode Practice

---
+++ 0201_0300/243_Shortest_Word_Distance.cpp
@@ -0,0 +1,109 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// Given an array of strings wordsDict and two different strings that
+// already exist in the array word1 and word2, return the shortest
+// distance between these two words in the list.
+
+class Solution {
+public:
+	int shortestDistance(vector<string>& wordsDict, string word1, string word2) {
+        int n = wordsDict.size();
+        int sd = INT_MAX;
+        for(int i = 0; i < n; i++) {
+        	if(wordsDict[i] == word1) {
+        		for(int j = 0; j < n; j++) {
+        			if(wordsDict[j] == word2) sd = min(sd, abs(i - j));
+        		}
+        	}
+        }
+        return sd;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	int w; cin >> w;
+	vs wordsDict(w);
+	for(int i = 0; i < w; i++) cin >> wordsDict[i];
+	string word1, word2;
+	cin >> word1 >> word2;
+	
+	Solution *soln = new Solution();
+	cout << soln->shortestDistance(wordsDict, word1, word2);
+
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
---
+++ 0201_0300/244_Shortest_Word_Distance_2.cpp
@@ -0,0 +1,115 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// Design a data structure that will be initialized with a string array,
+// and then it should answer queries of the shortest distance between
+// two different strings from the array.
+// Implement the WordDistance class:
+// WordDistance(String[] wordsDict) initializes the object with the
+// strings array wordsDict.
+// int shortest(String word1, String word2) returns the shortest
+// distance between word1 and word2 in the array wordsDict.
+
+class WordDistance {
+private:
+	unordered_map<string, vector<int> > locations;
+public:
+	WordDistance(vector<string>& wordsDict) {
+		for(int i = 0; i < (int) wordsDict.size(); i++) {
+			locations[wordsDict[i]].push_back(i);
+		}
+    }
+    
+    int shortest(string word1, string word2) {
+    	int d = INT_MAX;
+    	for(auto i : locations[word1]) {
+    		for(auto j : locations[word2]) {
+    			d = min(d, abs(i - j));
+    		}
+    	}
+    	return d;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	
+	
+	Solution *soln = new Solution();
+
+
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
