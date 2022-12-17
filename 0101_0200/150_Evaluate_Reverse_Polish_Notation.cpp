a27510f0edbd04befa2dbe42e3fb7261f302dd77
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-12-17 14:02:29 +0530

    LeetCode Practice

---
+++ 0101_0200/150_Evaluate_Reverse_Polish_Notation.cpp
@@ -0,0 +1,124 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+// Valid operators are +, -, *, and /. Each operand may be an integer or another expression.
+// Note that division between two integers should truncate toward zero.
+// It is guaranteed that the given RPN expression is always valid. That means the expression would always evaluate to a result, and there will not be any division by zero operation.
+
+class Solution {
+private:
+	bool isOperator(string t) {
+		return (t == "+" || t == "-" || t == "*" || t == "/");
+	}	
+
+	int eval(string t, int n1, int n2) {
+		if(t == "+") return (n2 + n1);
+		else if(t == "-") return (n2 - n1);
+		else if(t == "*") return (n2 * n1);
+		else return (n2 / n1);
+	}
+
+public:
+	int evalRPN(vector<string>& tokens) {
+        stack<int> stk;
+        for(auto token : tokens) {
+        	if(isOperator(token)) {
+        		int n1 = stk.top();
+        		stk.pop();
+        		int n2 = stk.top();
+        		stk.pop();
+        		int e = eval(token, n1, n2);
+        		stk.push(e);
+        	} else {
+        		stk.push(stoi(token));
+        	}
+        }
+        return stk.top();
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	int t; cin >> t;
+	vs tokens(t);
+	for(int i = 0; i < t; i++) cin >> tokens[i];
+	
+	Solution *soln = new Solution();	
+	cout << soln->evalRPN(tokens);
+
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
