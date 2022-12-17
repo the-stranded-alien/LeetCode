e93ed502e89cac9738ef65ba01a7d11c10bebb5b
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-12-18 00:25:03 +0530

    LeetCode Practice

---
+++ 1001_1100/1003_Check_if_Word_is_Valid_After_Substitutions.cpp
@@ -0,0 +1,117 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// Given a string s, determine if it is valid.
+// A string s is valid if, starting with an empty string t = "",
+// you can transform t into s after performing the following operation
+// any number of times: Insert string "abc" into any position in t.
+// More formally, t becomes tleft + "abc" + tright, where t ==
+// tleft + tright. Note that tleft and tright may be empty.
+// Return true if s is a valid string, otherwise, return false.
+
+class Solution {
+public:
+	// TLE
+	// bool isValid(string s) {
+    // 	while(!s.empty()) {
+    // 		string oldS = s;
+    // 		for(int i = 0; i < (int) s.length() - 2; i++) {
+    // 			if(s.substr(i, 3) == "abc") {
+    // 				s = s.substr(0, i) + s.substr(i + 3);
+    // 			}
+    // 		}
+    // 		if(oldS == s) break;
+    // 	}   
+    // 	return s.empty();
+    // }
+    bool isValid(string s) {
+    	string tmp = s;
+    	auto position = string::npos;
+    	// auto position = 0;
+        while((position = tmp.find("abc", 0)) != string::npos) tmp.erase(position, 3);   
+        return tmp.empty();
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	string s; cin >> s;
+	
+	Solution *soln = new Solution();
+	cout << soln->isValid(s);
+
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
