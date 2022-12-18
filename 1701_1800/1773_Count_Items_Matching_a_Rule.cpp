8a404e3c907996e4ed665a464e25e3085e45a486
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-12-18 20:11:14 +0530

    LeetCode Practice

---
+++ 1701_1800/1773_Count_Items_Matching_a_Rule.cpp
@@ -0,0 +1,116 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// You are given an array items, where each items[i] =
+// [typei, colori, namei] describes the type, color, and
+// name of the ith item. You are also given a rule represented
+// by two strings, ruleKey and ruleValue.
+// The ith item is said to match the rule if one of the
+// following is true:
+// ruleKey == "type" and ruleValue == typei.
+// ruleKey == "color" and ruleValue == colori.
+// ruleKey == "name" and ruleValue == namei.
+// Return the number of items that match the given rule.
+
+class Solution {
+public:
+	int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+        int matches = 0;
+        for(auto item : items) {
+        	string type = item[0];
+        	string color = item[1];
+        	string name = item[2];
+        	if(ruleKey == "type" && ruleValue == type) matches++;
+        	else if(ruleKey == "color" && ruleValue == color) matches++;
+        	else if(ruleKey == "name" && ruleValue == name) matches++;
+        }
+        return matches;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	int n; cin >> n;
+	vector<vector<string> > items(n, vector<string>(3));
+	for(int i = 0; i < n; i++) cin >> items[i][0] >> items[i][1] >> items[i][2];
+	string ruleKey; cin >> ruleKey;
+	string ruleValue; cin >> ruleValue;
+	
+	Solution *soln = new Solution();
+	cout << soln->countMatches(items, ruleKey, ruleValue);
+
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
