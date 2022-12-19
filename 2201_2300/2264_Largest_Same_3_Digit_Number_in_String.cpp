00660ef7c9638e28a064b6ffd17e574c2342d525
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-12-19 14:35:53 +0530

    LeetCode Practice

---
+++ 2201_2300/2264_Largest_Same_3_Digit_Number_in_String.cpp
@@ -0,0 +1,119 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// You are given a string num representing a large integer.
+// An integer is good if it meets the following conditions:
+// It is a substring of num with length 3.
+// It consists of only one unique digit.
+// Return the maximum good integer as a string or an empty
+// string "" if no such integer exists.
+// Note:
+// A substring is a contiguous sequence of characters within
+// a string. There may be leading zeroes in num or a good integer.
+
+class Solution {
+public:
+	string largestGoodInteger(string num) {
+        int l = num.length();
+        char mxCh = '0';
+        string ans = "";
+        for(int i = 0; i <= (l - 3); i++) {
+        	char c1 = num[i];
+        	char c2 = num[i + 1];
+        	char c3 = num[i + 2];
+        	if(c1 == c2 && c2 == c3 && c3 == c1) {
+        		if(c1 >= mxCh) {
+        			ans = "";
+        			mxCh = c1;
+        			ans += mxCh;
+        			ans += mxCh;
+        			ans += mxCh;
+        		}
+        	}
+        }
+        return ans;
+    }
+};
+
+void solve() {
+	// Main Code Goes Here !!
+	string num; cin >> num;
+	
+	Solution *soln = new Solution();
+	cout << soln->largestGoodInteger(num);
+
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
